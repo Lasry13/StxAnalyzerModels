0de141d8ffd913ee761d09c282956fa82a704c76
Lasry13 <39860898+Lasry13@users.noreply.github.com>
2020-06-08 19:15:28 +0300

    Delete Config.java

--- stxAnalyzer/src/main/java/co/stxanalyzer/Config.java
+++
@@ -1,8 +0,0 @@
-package co.stxanalyzer;
-
-public class Config {
-    public final static String applicationID = "stxAnalyzer";
-    public final static String bootstrapServers = "localhost:9092";
-    public final static String topicName = "raw_data";
-    public final static int numEvents = 10;
-}
